2b3abba9ee3d1d4c50575708591ad00345128b46
Alin Balasa <balasaalin@gmail.com>
2018-08-08 12:42:30 +0300

    Sample WH GTM Publishing Template and output.

---
+++ wh-gtm-output/oxygen-webhelp/app/nav-links/json/care-d66e27.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Pruning","href":"tasks\/pruning.html","attributes": {"data-id":"prunning",},"menu": {"hasChildren":false,},"tocID":"prunning-d66e38","topics":[]},{"title":"Garden Preparation","href":"tasks\/gardenPreparation.html","attributes": {"data-id":"gardenPreparation",},"menu": {"hasChildren":false,},"tocID":"gardenPreparation-d66e46","topics":[]}]});
